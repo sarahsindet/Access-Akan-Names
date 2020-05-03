f67100c5f471a52d19298bcf421842758789176e
sarahsindet <sarahsindet@gmail.com>
2020-05-04 02:05:06 +0300

    addes a README.md

---
+++ README.md
@@ -0,0 +1,70 @@
+# Akan Names
+#### Akan-names website, Friday 1st May
+#### By **Sarah Sindet**
+## Description
+github repository :https://github.com/sarahsindet/Access-Akan-Names
+In Ghana, people name their children after the day of the week they were born. The names are known as the Akan Names. This is a web application that takes a user's birthday, calculates the day of the week they were born and then depending on their gender outputs their Akan Name.
+
+## Setup/Installation Requirements
+* Click on the github repository link in the description above to get to my repository
+* Open the repository
+* Fork the repository
+* Then click on the clone/download button to get a link to clone or download 
+* pull the repository link to your local host
+* To open the live site click on the gh-page link in description.
+
+## Known Bugs 
+The formula for calculating the day of the week had an error but i plan to work on it after perfect research on what the correct formula should be.
+
+## Technologies Used
+ <!-- To develop this webpage i used vs-code as my text editor.The site is built on HTML, CSS, Javascript and Bootstrap. -->
+* HTML
+* CSS
+* Javascript
+* Bootstrap
+
+
+## BDD
+##### Input
+Year- The year of birth
+
+Month- The month of the year of birth
+
+Date- The day of the month
+
+##### Output
+Day of birth- The day of the week that the user was born eg Tuesday
+
+Akan name- The corresponding Akan name for the user eg.Kofi
+
+## Sample
+![image](image1.jpg)
+
+## Support and contact details
+ Incase you come across errors, have questions, ideas ,concerns, or want to contribute to the page, feel free to reach me at :cynthiaobu940@gmail.com
+ 
+ ### License
+  MIT - <https://opensource.org/licenses/MIT>
+  MIT License
+
+Copyright (c) 2020 Sarah Sindet
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+
+
+  
--- js/script.js
+++ js/script.js
@@ -1,32 +1,77 @@
 function submition(){
-    alert("See your Akan name??");
-}
+
 //*Declairing Variables*//
 var day = document.getElementById("day").value;
-var DD = parseInt(day);
+var dd = parseInt(day);
 var month = document.getElementById("month").value;
-var MM = parseInt(month);
+var maleNames = parseInt(month);
 var year = document.getElementById("year").value;
-var YY = parseInt(year.toString().slice(2, 4));
+var yy = parseInt(year.toString().slice(2, 4));
 //  var CC=(YY-1)/100+1;
-var CC = Math.ceil(year / 100)
+var cc = Math.ceil(year / 100)
 /* the formula */
-var process = parseInt ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7; 
+var process =  parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 if (document.getElementById("gender").checked) {
     var gender = 'male';
   }
   else {
     var gender = 'female';
   }
-  if (DD <= 0 || DD > 31) {
+  if (dd <= 0 || dd > 31) {
     alert("invalid date");
   }
-  else if (MM <= 0 || MM > 12) {
+  else if (mm <= 0 || mm > 12) {
     alert("invalid month");
   }
-  else if (MM == 2 && DD > 29) {
+  else if (mm == 2 && dd > 29) {
     alert("invalid day of month")
+  } 
+  else if (Math.round(process) == 0 && gender === 'male') {
+    document.getElementById("endresult").innerHTML = ("You were born on Sunday, your Akan Name is " + maleNames[0]);
   }
+  else if (Math.round(process) == 1 || Math.round(Process) == -1 && gender === 'male') {
+    document.getElementById("endresult").innerHTML = ("You were born on Monday, your Akan Name is " + maleNames[1]);
+  }
+  else if (Math.round(process) == 2 || Math.round(process) == -2 && gender === 'male') {
+    document.getElementById("endresult").innerHTML = ("You were born on Tuesday, your Akan Name is " + maleNames[2]);
+  }
+  else if (Math.round(process) == 3 || Math.round(process) == -3 && gender === 'male') {
+    document.getElementById("endresult").innerHTML = ("You were born on Wednesday, your Akan Name is " + maleNames[3]);
+  }
+  else if (Math.round(process) == 4 || Math.round(process) == -4 && gender === 'male') {
+    document.getElementById("endresult").innerHTML = ("You were born on Thursday, your Akan Name is " + maleNames[4]);
+  }
+  else if (Math.round(process) == 5 || Math.round(process) == -5 && gender === 'male') {
+    document.getElementById("endresult").innerHTML = ("You were born on Friday, your Akan Name is " + maleNames[5]);
+  }
+  else if (Math.round(process) == 6 || Math.round(process) == -6 && gender === 'male') {
+    document.getElementById("endresult").innerHTML = ("You were born on Saturday, your Akan Name is " + maleNames[6]);
+  }
+  else if (Math.round(process) == 0 && gender === 'female') {
+    document.getElementById("endresult").innerHTML = ("You were born on Sunday, your Akan name is " + femaleNames[0]);
+  }
+  else if (Math.round(process) == 1 || Math.round(process) == -1 && gender === 'female') {
+    document.getElementById("endresult").innerHTML = ("You were born on Monday, your Akan Name is " + femaleNames[1]);
+  }
+  else if (Math.round(process) == 2 || Math.round(process) == -2 && gender === 'female') {
+    document.getElementById("endresult").innerHTML = ("You were born on Tuesday, your Akan Name is " + femaleNames[2]);
+  }
+  else if (Math.round(process) == 3 || Math.round(process) == -3 && gender === 'female') {
+    document.getElementById("endresult").innerHTML = ("You were born on Wednesday, your Akan Name is " + femaleNames[3]);
+  }
+  else if (Math.round(process) == 4 || Math.round(process) == -4 && gender === 'female') {
+    document.getElementById("endresult").innerHTML = ("You were born on Thursday, your Akan Name is " + femaleNames[4]);
+  }
+  else if (Math.round(process) == 5 || Math.round(process) == -5 && gender === 'female') {
+    document.getElementById("endresult").innerHTML = ("You were born on Friday, your Akan Name is " + femaleNames[5]);
+  }
+  else if (Math.round(process) == 6 || Math.round(process) == -6 && gender === 'female') {
+    document.getElementById("endresult").innerHTML = ("You were born on Saturday, your Akan Name is " + femaleNames[6]);
+  }
+  else {
+    alert("please input all data");
+  }
+
+}
